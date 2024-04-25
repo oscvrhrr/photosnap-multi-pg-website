a9426d79acb69f717fb9ec4386c7c5f6fa60856c
Oscar Herrera <a.oscarherrera@gmail.com>
2024-04-25 14:56:28 -0500

    restructured dir

---
+++ main.js
@@ -0,0 +1,8 @@
+
+
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+const navMenu = document.querySelector('.nav-menu');
+
+hamburgerMenu.addEventListener('click', () => {
+    navMenu.classList.toggle('hide');
+})
