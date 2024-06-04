828b70df33f67038dcde9c6826582644b8ece534
Oscar Herrera <a.oscarherrera@gmail.com>
2024-06-04 15:43:19 -0500

    added capsule feature

--- main.js
+++ main.js
@@ -5,6 +5,25 @@ hamburgerMenu.addEventListener("click", () => {
   navMenu.classList.toggle("hide");
 });
 
+const switchBall = document.getElementsByClassName("switch-ball")[0];
+const capsule = document.getElementsByClassName("switch__capsule")[0];
+
+
+let state = false;
+
+capsule.addEventListener('click', () => {
+
+    if (state) {
+      switchBall.classList.remove("switch-ball--turn-on");
+      state = false;
+    } else {
+      switchBall.classList.add("switch-ball--turn-on")
+      state = true;
+    }
+
+});
+
+
 
 
 
